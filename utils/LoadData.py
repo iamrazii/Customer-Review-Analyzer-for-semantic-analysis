1197d446478201b46890ccf81bb31f5564c83e6a
unknown <razi.baba1019@gmail.com>
2025-10-11 15:27:30 +0500

    Selection of  random and limited entries from dataset

--- utils/LoadData.py
+++ utils/LoadData.py
@@ -5,4 +5,13 @@ def loadfile(path:str):
     df.dropna(subset=["Summary"])
     df["Summary"] = df["Summary"].astype(str)
 
-    return df
+    df_reduced = (
+    df.groupby('product_name', group_keys=False)
+      .apply(lambda x: x.loc[x.sample(frac=0.005, random_state=42).index])
+      .reset_index(drop=True)
+    )
+
+
+
+
+    return df_reduced
