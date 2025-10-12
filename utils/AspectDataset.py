2b19073c71d28197d82de373d2f0d2b90aa6000d
unknown <razi.baba1019@gmail.com>
2025-10-12 15:11:14 +0500

    Creating Aspect Dataset and assigning product ids to major dataframe

---
+++ utils/AspectDataset.py
@@ -0,0 +1,30 @@
+
+
+import pandas as pd
+
+def generate_aspect_dataset(df: pd.DataFrame):
+    data = []
+    for _, row in df.iterrows():
+        product_id = row["product_id"]
+        entries = row["aspect_sentiments"]  # list of dicts
+
+        if not entries or not isinstance(entries, list):
+            continue
+
+        for entry in entries:
+            aspect = entry.get("aspect")
+            opinion = entry.get("opinion")
+            sentiment = entry.get("sentiment")
+            phrase = f"{opinion} {aspect}"
+
+            data.append({
+                "product_id":product_id,
+                "aspect": aspect,
+                "opinion": opinion,
+                "phrase":phrase,
+                "sentiment": sentiment
+            })
+
+    new_df = pd.DataFrame(data)
+    new_df.to_csv("data/aspect_data.csv",index=False)
+    return new_df
--- utils/LoadData.py
+++ utils/LoadData.py
@@ -5,12 +5,17 @@ def loadfile(path:str):
     df.dropna(subset=["Summary"])
     df["Summary"] = df["Summary"].astype(str)
 
+    unique_products = df["product_name"].unique()
+    product_id_map = {name: idx + 100 for idx, name in enumerate(unique_products)}
+
+    # Step 2 — map those IDs back into the dataframe
+    df["product_id"] = df["product_name"].map(product_id_map)
+    
     df_reduced = (
     df.groupby('product_name', group_keys=False)
-      .apply(lambda x: x.loc[x.sample(frac=0.005, random_state=42).index])
+      .sample(frac=0.005, random_state=42)
       .reset_index(drop=True)
     )
-
 
 
 
