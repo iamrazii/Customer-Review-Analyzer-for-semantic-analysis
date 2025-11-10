63a33eb4840941bf4f6ed78e2dc378ab2f70d26d
unknown <razi.baba1019@gmail.com>
2025-11-11 01:01:33 +0500

    Updating data processing for ABSA

--- utils/DataProcessing.py
+++ utils/DataProcessing.py
@@ -3,23 +3,57 @@ from nltk import pos_tag , word_tokenize
 import re
 from nltk.corpus import stopwords
 
-# nltk.download("punkt", quiet=True) # tokenizer
-# nltk.download("averaged_perceptron_tagger_eng", quiet=True) # for POS
+# Run below 3 lines once, then comment it  
+nltk.download("punkt", quiet=True) # tokenizer
+nltk.download("averaged_perceptron_tagger_eng", quiet=True) # for POS
+nltk.download('stopwords')
 
-def CleanText(text):
-    text = re.sub(r"[^a-zA-Z\s]", "", text)
-    return " ".join([word.lower() for word in text.split() if word.lower() not in stopwords])
 
 def AspectExtraction(text):
+    
+    NEGATIONS = {"no", "not", "never", "none", "hardly", "barely", "cannot", "can't", "don't", "doesn't", "didn't"}
+    CONNECTORS_TO_KEEP = {'and', 'but', 'or', 'nor', 'with', 'for', 'on', 'at', 'to', 'from', 'about', 'by', 'as', 'than'}
+    DEFAULT_STOPWORDS = set(stopwords.words("english"))
+    STOPWORDS = (DEFAULT_STOPWORDS - NEGATIONS) - CONNECTORS_TO_KEEP 
+    # adding spaces between word and adjacent terminal(./,)
+    text = re.sub(r'([.,])', r' \1 ', text)
+    text = re.sub(r'\s{2,}', ' ', text).strip() 
+    # Tokenization
     tokens = word_tokenize(text)
-    tagged = pos_tag(tokens) # assigning parts of speech to each token in form of tuple (token,POS)
+    filtered_tokens = [t for t in tokens if t.lower() not in STOPWORDS]
+    tagged = pos_tag(filtered_tokens)
     aspects = []
 
-    for i , (word,tag) in enumerate(tagged):
+    for i, (word, tag) in enumerate(tagged):
+        #  ADJECTIVE + NOUN patterns 
         if tag.startswith("NN"):
-            if i>0 and tagged[i-1][1].startswith("JJ"): # if previous token is adjective
-                aspects.append((tagged[i-1][0],word))
-            elif i+1 < len(tagged) and tagged[i+1][1].startswith("JJ"):
-                aspects.append((word, tagged[i+1][0]))  # (noun, adj)
-    return aspects  
-    
+            
+            # Adjective + Noun (JJ NN)
+            if i > 0 and tagged[i - 1][1].startswith("JJ"):
+                opinion = tagged[i - 1][0]
+                
+                # Handle negation 
+                if i - 2 >= 0 and tagged[i - 2][0].lower() in NEGATIONS:
+                    combined_adj = f"{tagged[i - 2][0]} {opinion}"
+                    aspects.append((combined_adj, word))
+                else:
+                    aspects.append((opinion, word)) 
+
+            # Noun + Adjective (NN JJ)
+            elif i + 1 < len(tagged) and tagged[i + 1][1].startswith("JJ"):
+                opinion = tagged[i + 1][0]
+                aspects.append((opinion, word)) # (Opinion, Aspect)
+
+        # VERB/NOUN + ADVERB pattern (VB/NN RB) ---
+        if tag.startswith(("VB", "NN")):
+            if i + 1 < len(tagged) and tagged[i + 1][1].startswith("RB"):
+                opinion = tagged[i + 1][0]
+                aspect = word
+                # Handle negation (less common, but robust)
+                if i - 1 >= 0 and tagged[i - 1][0].lower() in NEGATIONS:
+                    combined_adv = f"{tagged[i - 1][0]} {opinion}"
+                    aspects.append((combined_adv, aspect)) 
+                else:
+                    aspects.append((opinion, aspect))
+                
+    return aspects
